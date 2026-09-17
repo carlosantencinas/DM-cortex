1c697909bc2366c0308d934a5967765137c5060d
carlosantencinas <64339850+carlosantencinas@users.noreply.github.com>
2026-09-16 22:48:36 -0400

    feat: add selectable character creation options

---
+++ src/data/characterOptions.js
@@ -0,0 +1,29 @@
+export const CLASS_OPTIONS = [
+  'Bárbaro', 'Bardo', 'Clérigo', 'Druida', 'Guerrero', 'Monje',
+  'Paladín', 'Explorador', 'Pícaro', 'Hechicero', 'Brujo', 'Mago'
+];
+
+export const SPECIES_OPTIONS = [
+  'Aasimar', 'Dracónido', 'Enano', 'Elfo', 'Gnomo', 'Goliat',
+  'Mediano', 'Humano', 'Orco', 'Tiefling'
+];
+
+export const BACKGROUND_OPTIONS = [
+  'Acólito', 'Artesano', 'Artista', 'Charlatán', 'Criminal',
+  'Ermitaño', 'Guardia', 'Marinero', 'Noble', 'Sabio', 'Soldado', 'Viajero'
+];
+
+export const ALIGNMENT_OPTIONS = [
+  'Legal bueno', 'Neutral bueno', 'Caótico bueno',
+  'Legal neutral', 'Neutral', 'Caótico neutral',
+  'Legal malvado', 'Neutral malvado', 'Caótico malvado'
+];
+
+export const SIZE_OPTIONS = ['Pequeño', 'Mediano', 'Grande'];
+
+export const ABILITY_GENERATION_OPTIONS = [
+  { value: 'standard', label: 'Arreglo estándar' },
+  { value: 'point-buy', label: 'Compra de puntos' },
+  { value: 'rolled', label: 'Tiradas manuales' },
+  { value: 'manual', label: 'Asignación manual' }
+];
